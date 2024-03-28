9c7a6d1f1a6870a32d8767a6bfc0a01e809ad448
e1327906 <e1327906@u.nus.edu>
2024-03-28 21:01:42 +0800

    Added changes for ticket validation usage api and otp api

---
+++ tg/tg_query_api/src/main/java/com/qre/tg/query/api/common/Constants.java
@@ -0,0 +1,15 @@
+package com.qre.tg.query.api.common;
+
+
+public class Constants {
+    public static final String BASE_URL = "/api/v1";
+
+    public static final String REQUESTER = "REQUESTER";
+    public static final String QR_TYPE = "QR_TYPE";
+    public static final int REQUESTER_KEY = 1;
+    public static final int QR_TYPE_KEY = 2;
+
+    public static final String X_API_KEY = "X-API-KEY";
+    public static final String VALID_API_KEY = "4f9d5fe0-a964-4f11-af99-6c40de98af77";
+    public static final String JWT_SECRET = "bezKoderSecretKey";
+}
