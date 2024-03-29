9eb7b0c164efd68da32e1255e907777fbbdacfcd
yoonmiemieaung <e0941690@u.nus.edu>
2024-03-29 08:25:32 +0630

    add test cases

--- tg/tg_query_api/src/test/java/com/qre/tg/query/api/repository/TicketMasterRepositoryTest.java
+++ tg/tg_query_api/src/test/java/com/qre/tg/query/api/repository/TicketMasterRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.qre.tg.query.api.repository;
 
 import com.qre.tg.dao.ticket.TicketMasterRepository;
+import com.qre.tg.dto.qr.TicketDetailResponse;
 import com.qre.tg.entity.ticket.TicketMaster;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
@@ -20,5 +25,26 @@ public class TicketMasterRepositoryTest {
         when(ticketMasterRepository.save(mockTicketMaster)).thenReturn(mockTicketMaster);
         TicketMaster savedticketMaster = ticketMasterRepository.save(mockTicketMaster);
         assertNotNull(savedticketMaster,"ticket master is not saved successfully");
+    }
+
+    @Test
+    public void TicketMasterRepository_findAllByEmail_ReturnMatchingTicketMaster () {
+        List<TicketMaster> ticketList = ticketMasterRepository.findAllByEmail("younmemeaung@gmail.com");
+        Assertions.assertThat(ticketList).isNotNull();
+        Assertions.assertThat(ticketList).hasSizeGreaterThan(0);
+    }
+
+    @Test
+    public void TicketMasterRepository_findAllBySerialNumber_ReturnMatchingTicketMaster () {
+        Optional<TicketMaster> ticketList = ticketMasterRepository.findAllBySerialNumber("test_serial_number");
+        Assertions.assertThat(ticketList).isNotNull();
+        Assertions.assertThat(ticketList).isNotEmpty();
+    }
+
+    @Test
+    public void TicketMasterRepository_findByTransactionDataPaymentRefNo_ReturnMatchingTicketMaster () {
+        Optional<TicketMaster> ticketList = ticketMasterRepository.findByTransactionDataPaymentRefNo("test_transaction_data_payment_number");
+        Assertions.assertThat(ticketList).isNotNull();
+        Assertions.assertThat(ticketList).isNotEmpty();
     }
 }
