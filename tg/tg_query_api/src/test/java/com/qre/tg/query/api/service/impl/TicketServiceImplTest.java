d4cbd892a2b8b4b41e2467d97441630f80b482d8
yoonmie12 <e0941690@u.nus.edu>
2024-04-26 20:04:10 +0800

    Update TicketServiceImplTest.java

--- tg/tg_query_api/src/test/java/com/qre/tg/query/api/service/impl/TicketServiceImplTest.java
+++ tg/tg_query_api/src/test/java/com/qre/tg/query/api/service/impl/TicketServiceImplTest.java
@@ -4,24 +4,26 @@ import com.qre.cmel.exception.ExceptionMsg;
 import com.qre.tg.dao.ticket.TicketMasterRepository;
 import com.qre.tg.dao.user.RoleRepository;
 import com.qre.tg.dao.user.UserRepository;
+import com.qre.tg.dto.auth.AuthenticationRequest;
 import com.qre.tg.dto.auth.AuthenticationResponse;
 import com.qre.tg.dto.qr.PurchaseTicketRequest;
+import com.qre.tg.dto.qr.TicketDetailResponse;
 import com.qre.tg.dto.user.UserRequest;
-import com.qre.tg.entity.ticket.AdditionalInfo;
-import com.qre.tg.entity.ticket.TicketMaster;
-import com.qre.tg.entity.ticket.TransactionData;
-import com.qre.tg.entity.user.Role;
+import com.qre.tg.entity.ticket.*;
 import com.qre.tg.entity.user.RoleType;
-import com.qre.tg.entity.user.User;
-import com.qre.tg.entity.ticket.Security;
+import com.qre.tg.query.api.common.TicketStatusEnum;
+import com.qre.tg.query.api.config.ApplicationProperties;
+import com.qre.tg.query.api.config.JwtService;
+import com.qre.tg.query.api.controller.impl.AuthenticationControllerImpl;
 import com.qre.tg.query.api.controller.impl.TicketServiceControllerImpl;
-import com.qre.tg.query.api.service.TicketService;
 
+
+import org.aspectj.lang.annotation.Before;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
@@ -29,12 +31,9 @@ import java.util.*;
 
 import javax.mail.MessagingException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class TicketServiceImplTest {
 
@@ -46,7 +45,19 @@ class TicketServiceImplTest {
     private TicketServiceControllerImpl ticketServiceController;
 
     @Mock
+    private AuthenticationServiceImpl authService;
+
+    @InjectMocks
+    private AuthenticationControllerImpl authController;
+
+    @Mock
     private TicketMasterRepository ticketMasterRepository;
+    
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private ApplicationProperties applicationProperties;
 
     @Mock
     private UserRepository userRepository;
@@ -57,40 +68,16 @@ class TicketServiceImplTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
+    private PurchaseTicketRequest purchaseTicketRequest;
 
+    private TicketMaster ticketMaster;
+
+    private Long currentTime = new Date().getTime();
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-    }
 
-    @Test
-    public void testPurchaseTicket() throws Exception{
-        Long currentTime = new Date().getTime();
-
-        // UserRequest userRequest = UserRequest.builder()
-        //         .userName("zaw")
-        //         .email("zaw@gmail.com")
-        //         .phoneNumber("1122334455")
-        //         .password("zaw")
-        //         .role(RoleType.ROLE_USER.name())
-        //         .build();
-        // Role role = new Role();
-        // role.setName(RoleType.ROLE_USER);
-        // when(roleRepository.findRoleByName(RoleType.ROLE_USER)).thenReturn(role);
-        // Set<Role> roles = new HashSet<>();
-        // roles.add(role);
-
-        // User user = User.builder()
-        //         .userName(userRequest.getUserName())
-        //         .email(userRequest.getEmail())
-        //         .phoneNumber(userRequest.getPhoneNumber())
-        //         .password(passwordEncoder.encode(userRequest.getPassword()))
-        //         .roles(roles)
-        //         .build();
-        // // Stubbing userRepository.save() to accept any User object
-        // when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        PurchaseTicketRequest request = PurchaseTicketRequest.builder()
+        PurchaseTicketRequest purchaseTicketRequest = PurchaseTicketRequest.builder()
                 .journeyType(1)
                 .groupSize(0)
                 .operatorId(3)
@@ -101,64 +88,136 @@ class TicketServiceImplTest {
                 .paymentRefNo("PAY123")
                 .amount(1000L)
                 .currency("USD")
-                .phoneNo("+1234567890")
-                .email("test@example.com")
                 .build();
-
-        // TicketMaster ticketMaster = TicketMaster.builder()
-        //         .ticketId(1)
-        //         .ticketType(1)
-        //         .journeyTypeId(1)
-        //         .serialNumber(ticketService.generateSerialNumber(req))
-        //         .groupSize(1)
-        //         .creatorId(5)
-        //         .validityPeriod(0)
-        //         .operatorId(0)
-        //         .creationDateTime(currentTime)
-        //         .effectiveDateTime(currentTime + 3600000) // Adding 1 hour for testing
-        //         .issuerId(10)
-        //         .validityDomain(1)
-        //         .userId(1)
-        //         .phoneNo("")
-        //         .email("test@example.com")
-        //         .journeyDetails(ticketService.getJourneyDetails(req))
-        //         .qrData(prepareQRData)
-        //         .build();
-        //Date dd = new Date().getTime();
-         TicketMaster ticketMaster = TicketMaster.builder()
-                .ticketType(request.getTicketType())
-                .journeyTypeId(request.getJourneyType())
-                .serialNumber(ticketService.generateSerialNumber(request))
+        this.purchaseTicketRequest = purchaseTicketRequest;
+        TicketMaster ticketMaster = TicketMaster.builder()
+                .ticketType(purchaseTicketRequest.getTicketType())
+                .journeyTypeId(purchaseTicketRequest.getJourneyType())
+                .serialNumber(ticketService.generateSerialNumber(purchaseTicketRequest))
                 .issuerId(10)
                 .creatorId(5) //tg_id
-                .groupSize(request.getGroupSize())
-                .phoneNo(request.getPhoneNo())
-                .email(request.getEmail())
+                .groupSize(purchaseTicketRequest.getGroupSize())
                 .creationDateTime(new Date())
                 .effectiveDateTime(new Date())
-                .operatorId(request.getOperatorId()) // transport operator
+                .operatorId(purchaseTicketRequest.getOperatorId()) // transport operator
                 .validityDomain(1) // train/bus/flight/all
-                .validityPeriod(ticketService.getValidityPeriod(new Date(request.getStartDatetime()),
-                        new Date(request.getEndDatetime())))
+                .validityPeriod(ticketService.getValidityPeriod(new Date(purchaseTicketRequest.getStartDatetime()),
+                        new Date(purchaseTicketRequest.getEndDatetime())))
                 .transactionData(TransactionData.builder()
-                        .paymentRefNo(request.getPaymentRefNo())
-                        .currency(request.getCurrency())
-                        .amount(request.getAmount())
+                        .paymentRefNo(purchaseTicketRequest.getPaymentRefNo())
+                        .currency(purchaseTicketRequest.getCurrency())
+                        .amount(purchaseTicketRequest.getAmount())
                         .build())
-                .journeyDetails(ticketService.getJourneyDetails(request))
+                .journeyDetails(ticketService.getJourneyDetails(purchaseTicketRequest))
                 .additionalInfo(AdditionalInfo.builder().build())
                 .build();
+        ticketMaster.setCreatedDatetime(new Date());
+        this.ticketMaster =ticketMaster;
+    }
+
+
+    UserRequest authicateUser() throws MessagingException, IOException {
+        // Given
+        UserRequest userRequest = UserRequest.builder()
+                .userName("test")
+                .email("test@gmail.com")
+                .phoneNumber("1122334455")
+                .password("test")
+                .role(RoleType.ROLE_USER.name())
+                .build();
+        AuthenticationResponse mockResponse = AuthenticationResponse.builder()
+                .accessToken("fakeAccessToken")
+                .refreshToken("fakeRefreshToken")
+                .build();
+
+        when(authService.register(userRequest)).thenReturn(mockResponse);
+
+        // When
+        ResponseEntity<AuthenticationResponse> response = authController.register(userRequest);
+
+        AuthenticationRequest authReq = AuthenticationRequest.builder()
+                .email(userRequest.getEmail())
+                .password(userRequest.getPassword())
+                .build();
+
+        ResponseEntity<AuthenticationResponse> res = authController.authenticate(authReq);
+        System.out.println(authReq);
+        return userRequest;
+    }
+
+
+    @Test
+    public void testGuestUserCanPurchaseTicketSuccessful() throws Exception{
         Map<String, Object> ticketJsonData = ticketService.prepareTicketJsonData(ticketMaster);
         ticketMaster.setSecurity(Security.builder()
                 .digitalSignature(ticketService.generateDigitalSignature(ticketJsonData))
                 .build());
         ticketMaster.setQrData(ticketService.prepareQRData(ticketJsonData));
-        ticketMaster.setCreatedDatetime(new Date());
-
+        ticketMaster.setEmail("guest@email.com");
+        ticketMaster.setPhoneNo(null);
         when(ticketMasterRepository.save(any(TicketMaster.class))).thenReturn(ticketMaster);
         assertNotNull(ticketMaster);
-
     }
+
+    @Test
+    public void testRegiestedUserCanPurchaseTicketSuccessful() throws Exception{
+        UserRequest registedUser =  authicateUser();
+        Map<String, Object> ticketJsonData = ticketService.prepareTicketJsonData(ticketMaster);
+        ticketMaster.setSecurity(Security.builder()
+                .digitalSignature(ticketService.generateDigitalSignature(ticketJsonData))
+                .build());
+        ticketMaster.setQrData(ticketService.prepareQRData(ticketJsonData));
+        ticketMaster.setEmail(registedUser.getEmail());
+        ticketMaster.setPhoneNo(registedUser.getPhoneNumber());
+        when(ticketMasterRepository.save(any(TicketMaster.class))).thenReturn(ticketMaster);
+        assertNotNull(ticketMaster);
+    }
+
+//     @Test
+//     void testFindAllByEmail_ReturnTicketList() throws Exception {
+//         UserRequest registedUser =  authicateUser();
+//         purchaseTicketRequest.setEmail(registedUser.getEmail());
+
+//         List<JourneyDetails> journeyDetailsList = new ArrayList<>();
+
+//         JourneyDetails journeyDetails = JourneyDetails.builder()
+//                 .departurePoint(purchaseTicketRequest.getDeparturePoint())
+//                 .arrivalPoint(purchaseTicketRequest.getArrivalPoint())
+//                 .status(TicketStatusEnum.ACTIVE.getValue())
+//                 .build();
+//         journeyDetails.setCreatedDatetime(new Date());
+//         journeyDetailsList.add(journeyDetails);
+
+
+//         Map<String, Object> ticketJsonData = ticketService.prepareTicketJsonData(ticketMaster);
+//         ticketMaster.setSecurity(Security.builder()
+//                 .digitalSignature(ticketService.generateDigitalSignature(ticketJsonData))
+//                 .build());
+//         ticketMaster.setQrData(ticketService.prepareQRData(ticketJsonData));
+//         ticketMaster.setEmail(registedUser.getEmail());
+//         ticketMaster.setPhoneNo(registedUser.getPhoneNumber());
+//         ticketMaster.setJourneyDetails(journeyDetailsList);
+//         System.out.println(journeyDetailsList);
+
+
+//         when(ticketMasterRepository.save(any(TicketMaster.class))).thenReturn(ticketMaster);
+//         System.out.println(ticketMaster);
+
+//         List<TicketDetailResponse> ticketList = ticketService.findAllByEmail(registedUser.getEmail());
+//         System.out.println(ticketList);
+
+// //        Assertions.assertThat(ticketList).isNotNull();
+// //        Assertions.assertThat(ticketList).hasSizeGreaterThan(0);
+//     }
+
+//    @Test
+//    void getJourneyDetail_ReturnJourneyDetailList() {
+//        List<JourneyDetails> journeyDetailsList = ticketService.getJourneyDetails(request);
+//        Assertions.assertThat(journeyDetailsList).isNotNull();
+//        Assertions.assertThat(journeyDetailsList).hasSizeGreaterThan(0);
+//    }
+
+
   
    
 
